82a461bd66cf7bcd2d0fe1538892ad98cb315de9
nadellarajesh <nadellarajesh.n@gmail.com>
2022-03-14 15:39:00 +0530

    First commit

---
+++ src/test/java/com/bookstorerest/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookstorerest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
